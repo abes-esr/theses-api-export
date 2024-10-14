34384e72d34dfcddd6a312842cd886bf981e97b2
gibert <gibert@abes.fr>
2024-10-14 12:29:14 +0200

    bug : majuscule manquante dans la verification des params

--- src/main/java/fr/abes/theses/export/controller/OutilsController.java
+++ src/main/java/fr/abes/theses/export/controller/OutilsController.java
@@ -19,12 +19,11 @@ public class OutilsController {
     @Autowired
     DbRequest dbRequest;
 
-//    @GetMapping(value = "algoTheses",produces = "application/vnd.ms-excel")
     @GetMapping(value = "algoTheses")
     public ResponseEntity algoTheses(
             @RequestParam @Parameter(name = "etat", description = "Code du problème, multivalué possible", example = "52") Optional<List<Integer>> etat,
             @RequestParam @Parameter(name = "type", description = "Type d'intervention, multivalué possible", example = "amelioration") Optional<List<String>> type,
-            @RequestParam @Parameter(name = "codeetab", description = "Code de l'établissement", example = "NICE") Optional<List<String>> codeEtab,
+            @RequestParam @Parameter(name = "codeEtab", description = "Code de l'établissement", example = "NICE") Optional<List<String>> codeEtab,
             @RequestParam @Parameter(name = "nnt", description = "Numéro national de thèse", example = "2020MONT1234") Optional<List<String>> nnt,
             @RequestParam @Parameter(name = "ppn", description = "Identifiant de la notice", example = "236863509") Optional<List<String>> ppn,
             @RequestParam @Parameter(name = "utilisateur", description = "Type d'utilisateur", example = "reseau") Optional<List<String>> utilisateur,
